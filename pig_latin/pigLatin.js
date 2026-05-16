2cfc4336cc44a54cd1dc15fb1cfd05dcaf474e79
MarlaSinger2025 <Marla_Singer_2020@web.de>
2026-05-16 15:07:33 +0200

    first braindump ideas

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,6 +1,22 @@
+// Convert each word to Pig Latin:
+
+//     If a word starts with a consonant and a vowel, put the first letter of the word at the end of the word and add "ay."
+//         Example: Happy = appyh + ay = appyhay
+
+//     If a word starts with two consonants move the two consonants to the end of the word and add "ay."
+//         Example: Child = Ildch + ay = Ildchay
+
+//     If a word starts with a vowel add the word "way" at the end of the word.
+//         Example: Awesome = Awesome +way = Awesomeway
+
+
+let abc = [];
+let vowels = ['a', 'e', 'i', 'o', 'u'];
+let consonants = [''];
+
 let choices = ['rock', 'paper', 'scissors'];
 
-const playerChoice = process.argv.slice(2)[0];
+const text=process.argv.slice(2)[0];
 
 const computerChoice = choices[Math.floor(Math.random() * choices.length)];
     console.log(`Computer: ${computerChoice}`);
