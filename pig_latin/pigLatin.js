26705ded7970f7452a0661c198b628cfa80cc0c6
MarlaSinger2025 <Marla_Singer_2020@web.de>
2026-05-16 16:14:23 +0200

    Adding something to the end of the string if it starts with a vowel

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -13,38 +13,52 @@
 let abc = [];
 let vowels = ['a', 'e', 'i', 'o', 'u'];
 let consonants = [''];
+let toVowel = 'way';
 
-let choices = ['rock', 'paper', 'scissors'];
+// let choices = ['rock', 'paper', 'scissors'];
 
-const text=process.argv.slice(2)[0];
+const text = process.argv.slice(2)[0];
 
-const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    console.log(`Computer: ${computerChoice}`);
+// const translate = text.startsWith(vowels);
 
-if (!choices.includes(playerChoice)) {
-    console.error('You have to choose between rock, paper, or scissors as an input!');
+// if (text.startsWith('Hello')) { //checks if text starts with a specific word (How can I add the vowels variable instead?). Case Sesitive
+//     console.log(text.concat(toVowel)), //adds variable to the end of the word
+//     console.log(text.substring(1)) // returns part of string from start index up to the end of the string if no end index is supplied.
+// };
 
-} else if (  // Winning against Computer
-    (playerChoice === 'rock' && computerChoice === 'scissors') ||
-    (playerChoice === 'scissors' && computerChoice === 'paper') || 
-    (playerChoice === 'paper' && computerChoice === 'rock')
-){
-    console.log(
-        `You chose ${playerChoice}, and computer chose ${computerChoice}. You win!`
-    );
-
-} else if  ( // Loosing against Computer
-    (playerChoice === 'paper' && computerChoice === 'scissors') ||
-    (playerChoice === 'scissors' && computerChoice === 'rock') ||
-    (playerChoice === 'rock' && computerChoice === 'paper')
-) {
-    console.log(
-        `You chose ${playerChoice}, and computer chose ${computerChoice}. Computer wins, you loose! Sorry :(`
-    );
-} else if 
-    (playerChoice === computerChoice ) {
-    console.log(`You both chose ${playerChoice}, its a draw!`);
-    return;
+if (vowels.includes(text[0].toLowerCase())) { // determines whether a given string may be found within this string
+    console.log(text.substring(1).concat(toVowel))
 };
+
+
+
+// const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+//     console.log(`Computer: ${computerChoice}`);
+
+// if (!choices.includes(playerChoice)) {
+//     console.error('You have to choose between rock, paper, or scissors as an input!');
+
+// } else if (  // Winning against Computer
+//     (playerChoice === 'rock' && computerChoice === 'scissors') ||
+//     (playerChoice === 'scissors' && computerChoice === 'paper') || 
+//     (playerChoice === 'paper' && computerChoice === 'rock')
+// ){
+//     console.log(
+//         `You chose ${playerChoice}, and computer chose ${computerChoice}. You win!`
+//     );
+
+// } else if  ( // Loosing against Computer
+//     (playerChoice === 'paper' && computerChoice === 'scissors') ||
+//     (playerChoice === 'scissors' && computerChoice === 'rock') ||
+//     (playerChoice === 'rock' && computerChoice === 'paper')
+// ) {
+//     console.log(
+//         `You chose ${playerChoice}, and computer chose ${computerChoice}. Computer wins, you loose! Sorry :(`
+//     );
+// } else if 
+//     (playerChoice === computerChoice ) {
+//     console.log(`You both chose ${playerChoice}, its a draw!`);
+//     return;
+// };
 
 
